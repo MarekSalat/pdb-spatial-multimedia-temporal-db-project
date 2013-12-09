57af72ac7aa01ec9218cd93baaeb2932ba2dd823
admin <m1k3w0rks@gmail.com>
2013-12-09 21:42:05 +0100

    Základní Media Tab

--- src/main/java/cz/vutbr/fit/pdb/nichcz/model/media/MediaEntity.java
+++ src/main/java/cz/vutbr/fit/pdb/nichcz/model/media/MediaEntity.java
@@ -12,7 +12,26 @@ import java.util.UUID;
  * User: admin
  * Date: 6.12.13
  * Time: 16:24
- * To change this template use File | Settings | File Templates.
+ *
+ *
+ *
+
+ DROP TABLE PDB_MEDIA;
+
+ CREATE TABLE PDB_MEDIA
+ (
+     ID VARCHAR2(64) PRIMARY KEY NOT NULL,
+     NAME VARCHAR2(64),
+     PHOTO ORDSYS.ORDImage,
+     PHOTO_SI ORDSYS.SI_StillImage,
+     PHOTO_AC ORDSYS.SI_AverageColor,
+     PHOTO_CH ORDSYS.SI_ColorHistogram,
+     PHOTO_PC ORDSYS.SI_PositionalColor,
+     PHOTO_TX ORDSYS.SI_Texture
+ );
+
+ COMMIT;
+
  */
 public class MediaEntity implements Entity<Long> {
     public static String TABLE="PDB_MEDIA";
